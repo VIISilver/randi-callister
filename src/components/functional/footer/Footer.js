8a0545f9fdd16e8dd08d7105f8b1618d4e144654
VIISilver <jparke71@hotmail.com>
2019-03-13 00:32:51 -0600

    Content for footer has been added.

--- src/components/functional/footer/Footer.js
+++ src/components/functional/footer/Footer.js
@@ -1,44 +1,56 @@
-import React, { Component } from 'react';
-import withStyles from '@material-ui/core/styles/withStyles';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
-import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import './Footer.css';
 
 const logo = require('../../../assets/exercise.png');
+const facebook = require('../../../assets/social-facebook-box-blue-icon.png');
+const twitter = require('../../../assets/social-twitter.png');
+const instagram = require('../../../assets/social-instagram.png');
 
 const styles = theme => ({
-  footer: {
-    container: true,
+  root: {
     backgroundColor: theme.palette.grey['200'],
-    color: theme.palette.text.primary,
-    item: true,
-    direction: 'column',
-    alignItems: 'center',
-    alignContent: 'center',
-  }
+  },
+  logo: {
+    marginTop: 13,
+  },
+  tagline: {
+    display: 'inline-block',
+    marginLeft: 10
+  },
 })
 
-class Footer extends Component {
-  render() {
+class Footer extends React.Component {
+  state = {
+    value: 'recents',
+  };
 
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
     const { classes } = this.props;
+    const { value } = this.state;
 
     return (
-      <BottomNavigation>
-        <Grid container>
-          <Grid className={classes.footer} xs={6}>
-                    <Typography variant="h6" color="inherit" noWrap>
-            <img width={24} src={logo} />
-            </Typography>
-          </Grid>
-          <Grid className={classes.footer} xs={6}>
-            <p>Developed by Strength LLC</p>
-          </Grid>
-        </Grid>
+      <BottomNavigation value={value} onChange={this.handleChange} className={classes.root}>
+        <Typography variant="h6" color="inherit" noWrap>
+          <img width={24} src={logo} />
+          <span className={classes.tagline}>Developed by Strength LLC</span>
+        </Typography>
+          <a href="https://facebook.com" target="_blank"><img width={24} height={24} src={facebook} /></a>
+          <a href="https://twitter.com" target="_blank"><img width={24} height={24} src={twitter} /></a>
+          <a href="https://instagram.com" target="_blank"><img width={24} height={24} src={instagram} /></a>
       </BottomNavigation>
-    )
+    );
   }
 }
+
+Footer.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
 export default withStyles(styles)(Footer);
