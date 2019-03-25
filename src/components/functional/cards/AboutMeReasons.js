7b7fa66e89dce1c01b157340335fd033f89d438f
VIISilver <jparke71@hotmail.com>
2019-03-24 22:43:44 -0600

    Added more content to the AboutMeReasons.

--- src/components/functional/cards/AboutMeReasons.js
+++ src/components/functional/cards/AboutMeReasons.js
@@ -6,67 +6,80 @@ import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 
 const firstImage = require('../../../assets/image3.jpg');
-const secondImage = require('../../../assets/image4.jpg');
+const fourthImage = require('../../../assets/image6.jpg');
 
 const styles = theme => ({
-  itemContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-  },
-  media: {
-    // ⚠️ object-fit is not supported by IE 11.
-    objectFit: 'cover',
-  },
-  imageText: {
-    color: 'white',
-    position: 'absolute',
-    top: 77,
-  },
-  positionSetter: {
-    position: 'relative',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  textOverImage: {
-    padding: 0,
-    marginLeft: 20,
-  }
+    itemContainer: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+    },
+    media: {
+        // ⚠️ object-fit is not supported by IE 11.
+        objectFit: 'cover',
+    },
+    imageText: {
+        color: 'white',
+    },
+    positionSetter: {
+        position: 'relative',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    textOverImage: {
+        display: 'flex',
+        flexDirection: 'column',
+        padding: 0,
+        marginLeft: 20,
+        position: 'absolute',
+        top: 27,
+    },
+    reasonWrapper: {
+        padding: 30,
+    }
 })
 
 class AboutMeReasons extends Component {
 
-  render() {
-    const { classes } = this.props;
+    render() {
+        const { classes } = this.props;
 
-    return (
-      <Grid container xs={12} className={classes.itemContainer}>
-        <Grid item xs={12}>
-          <div className={classes.positionSetter}>
-            <CardMedia
-              component="img"
-              className={classes.media}
-              image={firstImage}
-            />
-            <CardHeader
-              className={classes.textOverImage}
-              title={
-                <Typography variant="h6" className={classes.imageText}>
-                  My Story
+        return (
+            <Grid container xs={12} className={classes.itemContainer}>
+                <Grid item xs={12}>
+                    <div className={classes.positionSetter}>
+                        <CardMedia
+                            component="img"
+                            className={classes.media}
+                            image={firstImage}
+                        />
+                        <div className={classes.textOverImage}>
+                            <Typography variant="h6" className={classes.imageText}>
+                                My Story
                   </Typography>
-              }
-            //   subheader={
-            //     <Typography variant="body2" className={classes.imageText}>
-            //       My Story
-            //       </Typography>
-            //   }
-            />
-          </div>
-        </Grid>
-      </Grid>
-    )
-  }
+                            <Typography variant="body2" className={classes.imageText}>
+                                Tempor duis id laboris Lorem adipisicing ad sunt reprehenderit. Aute enim laboris eiusmod velit. Voluptate commodo quis Lorem id est ea deserunt eu commodo elit commodo ipsum. Ipsum laborum nisi dolor est amet reprehenderit veniam consectetur consectetur irure deserunt.
+                  </Typography>
+                        </div>
+                    </div>
+                </Grid>
+                <Grid item xs={12} className={classes.reasonWrapper}>
+                    <Typography variant="h6">
+                        My Why
+                  </Typography>
+                    <Typography variant="body2">
+                        Incididunt nisi cupidatat nostrud culpa. Officia ullamco nulla cillum do velit sit duis ullamco occaecat cillum amet ut. Magna sint culpa nulla adipisicing excepteur ex.
+                  </Typography>
+                </Grid>
+                    <CardMedia
+                        component="img"
+                        className={classes.media}
+                        image={fourthImage}
+                    />
+            </Grid>
+        )
+    }
 }
 
 export default withStyles(styles)(AboutMeReasons);
